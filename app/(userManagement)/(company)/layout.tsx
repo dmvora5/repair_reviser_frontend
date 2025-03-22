51620320e2010165eff39325ba029c780351fe03
Pritesh Antala <priteshantala999@gmail.com>
2025-03-22 16:31:33 +0530

    uesr management login code comment

--- app/(userManagement)/(company)/layout.tsx
+++ app/(userManagement)/(company)/layout.tsx
@@ -6,14 +6,14 @@ import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 const ComapnyLayout = async ({ children }: { children: React.ReactNode }) => {
-  const session: any = await getServerSession(authOptions);
+  // const session: any = await getServerSession(authOptions);
 
-  if (session?.role !== ROLES.COMPANY_ADMIN) {
-    await signOut({
-      callbackUrl: `${process.env.NEXT_PUBLIC_APP_URL}${PAGE_ROUTES.AUTH.LOGIN}`,
-    });
-    return redirect(PAGE_ROUTES.AUTH.LOGIN);
-  }
+  // if (session?.role !== ROLES.COMPANY_ADMIN) {
+  //   await signOut({
+  //     callbackUrl: `${process.env.NEXT_PUBLIC_APP_URL}${PAGE_ROUTES.AUTH.LOGIN}`,
+  //   });
+  //   return redirect(PAGE_ROUTES.AUTH.LOGIN);
+  // }
 
   return children;
 };
